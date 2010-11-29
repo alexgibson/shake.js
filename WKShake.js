e5f07816ea25bce64c7cb8187e02b2d9a6a14d9a
Alex Gibson <a.gibson@me.com>
2010-11-29 19:42:38 +0000

    add feature detection

--- WKShake.js
+++ WKShake.js
@@ -31,17 +31,21 @@ WKShake.prototype.handleEvent = function(e) {
 
 //start listening for devicemotion
 WKShake.prototype.start = function() {
-	this.shakeOnce = false,
+	this.lastTime = new Date(),
 	this.lastX = null,
 	this.lastY = null,
 	this.lastZ = null;
-	window.addEventListener('devicemotion', this, false);
+	if (('ondevicemotion' in window)) {
+		window.addEventListener('devicemotion', this, false);
+	}
 };
 
 //stop listening for devicemotion
 WKShake.prototype.stop = function() {
-	window.removeEventListener('devicemotion', this, false);
-	this.shakeOnce = false,
+	if (('ondevicemotion' in window)) {
+		window.removeEventListener('devicemotion', this, false);
+	}
+	this.lastTime = new Date(),
 	this.lastX = null,
 	this.lastY = null,
 	this.lastZ = null;
@@ -55,16 +59,16 @@ WKShake.prototype.onDeviceMotion = function(e) {
 	if((this.lastX !== null) || (this.lastY !== null) || (this.lastZ !== null)) {
 
 		var deltaX = Math.abs(this.lastX - current.x),
-		deltaY = Math.abs(this.lastY - current.y),
-		deltaZ = Math.abs(this.lastZ - current.z);	
+			deltaY = Math.abs(this.lastY - current.y),
+			deltaZ = Math.abs(this.lastZ - current.z);	
 		
 		if(((deltaX > this.threshold) && (deltaY > this.threshold)) || 
-		((deltaX > this.threshold) && (deltaZ > this.threshold)) || 
-		((deltaY > this.threshold) && (deltaZ > this.threshold))) {
+		   ((deltaX > this.threshold) && (deltaZ > this.threshold)) || 
+		   ((deltaY > this.threshold) && (deltaZ > this.threshold))) {
 		
 			//calculate time in milliseconds since last shake registered
 			var currentTime = new Date(),
-			timeDifference = currentTime.getTime() - this.lastTime.getTime();
+				timeDifference = currentTime.getTime() - this.lastTime.getTime();
 			
 			if (timeDifference > 200) {
 				this.shakeEventDidOccur();	
